a78218ec18629242a1942d0fcbd1f457e253b05a
Amari Eyaa <amarieya4@gmail.com>
2025-11-30 13:29:34 +0100

    Initial commit

---
+++ src/main/java/tn/fst/tp5eyaammarigroupe3/Tp5EyaAmmariGroupe3Application.java
@@ -0,0 +1,13 @@
+package tn.fst.tp5eyaammarigroupe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5EyaAmmariGroupe3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp5EyaAmmariGroupe3Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/fst/tp5eyaammarigroupe3/Tp5EyaAmmariGroupe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.fst.tp5eyaammarigroupe3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5EyaAmmariGroupe3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
